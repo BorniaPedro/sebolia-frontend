7ca2e6235927e03b67f977696678bc1f08f1eca1
Pedro Bornia <borniaph@gmail.com>
2024-08-14 11:39:50 -0300

    Update rotas

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
 import React from "react";
 import { Route, Routes } from 'react-router-dom';
 import Home from "./components/Home";
-import Login from "./components/login";
+import Login from "./components/Login";
 import Teste from "./components/Teste";
+import CadastroLivro from "./components/CadastroLivro";
 import NavBar from './components/NavBar';
+import "./NavBar.css";
 
 function App() {
   return (
-    <div>
+    <div className="app-container">
       <NavBar />
       <Routes> 
-        <Route path="/" elemnt={<Home />} />
-        <Route path="/login" element={<Login />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/Login" element={<Login />} />
+        <Route path="/CadastroLivro" element={<CadastroLivro />} />
         <Route path="/Teste" element={<Teste />} />
       </Routes>
     </div>
