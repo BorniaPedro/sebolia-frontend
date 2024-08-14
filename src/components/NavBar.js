23a12735f3063926153f8fbac47d2d506ee59059
Pedro Bornia <borniaph@gmail.com>
2024-08-14 11:37:49 -0300

    Add componetes navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import "../NavBar.css";
 
 function NavBar() {
   return (
-    <nav>
-      <ul>
-        <li><Link to="/">Home</Link></li>
-        <li><Link to="/login">Login</Link></li>
-        <li><Link to="/Teste">Teste</Link></li>
+    <nav className="navbar">
+      <ul className="nav-links">
+        <li><Link to="/" className="nav-link">Home</Link></li>
+        <li><Link to="/Login" className="nav-link">Login</Link></li>
+        <li><Link to="/CadastroLivro" className="nav-link">Cadastro de Livro</Link></li>
+        <li><Link to="/Teste" className="nav-link">Teste</Link></li>
       </ul>
     </nav>
   );
