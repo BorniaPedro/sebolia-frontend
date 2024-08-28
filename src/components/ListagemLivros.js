746aac866ecf7eb1c9115ba04d50d1263ee7307d
Victor <victorgaspalmeida@hotmail.com>
2024-08-28 15:01:10 -0300

    feat: integrando backend com frontend listagem livros

--- src/components/ListagemLivros.js
+++ src/components/ListagemLivros.js
@@ -4,19 +4,16 @@ import "../styles/listagemLivros.css"
 
 function ListagemLivros() {
 
-  const [books] = useState([
-    { id: 1, titulo: "O senhor dos anéis: O retorno do rei", autor: "J. R. R. Tolkien", editora: "Darkside", ano: "1955" },
-    { id: 2, titulo: "Livro 2", autor: "Autor 2", editora: "Editora 2", ano: "Ano 2" },
-    { id: 3, titulo: "Nome do Livro 3", autor: "Autor 3", editora: "Editora 3", ano: "Ano 3" },
-    { id: 4, titulo: "Livro 4", autor: "Autor 4", editora: "Editora 4", ano: "Ano 4" },
-    { id: 5, titulo: "Livro 4", autor: "Autor 4", editora: "Editora 4", ano: "Ano 4" },
-    { id: 6, titulo: "Livro 4", autor: "Autor 4", editora: "Editora 4", ano: "Ano 4" },
-    { id: 7, titulo: "Livro 4", autor: "Autor 4", editora: "Editora 4", ano: "Ano 4" },
-    { id: 8, titulo: "Livro 4", autor: "Autor 4", editora: "Editora 4", ano: "Ano 4" },
-    { id: 9, titulo: "Livro 4", autor: "Autor 4", editora: "Editora 4", ano: "Ano 4" },
-    { id: 10, titulo: "Livro 4", autor: "Autor 4", editora: "Editora 4", ano: "Ano 4" },
+  const [books, setBooks] = useState([]);
 
-  ]);
+  const getLivros = async () => {
+    const url = "http://localhost:3500/livro";
+
+    const response = await fetch(url);
+  
+    const json = await response.json();
+    setBooks(json);
+  }
   
   const [showOptions, setShowOptions] = useState({});
 
@@ -34,6 +31,8 @@ function ListagemLivros() {
   };
 
   useEffect(() => {
+    getLivros();
+    
     document.addEventListener("mousedown", handleClickOutside);
     return () => {
       document.removeEventListener("mousedown", handleClickOutside);
