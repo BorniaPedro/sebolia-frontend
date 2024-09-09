8a38e22330c662882d33cd111bd1c19a56b7c5f0
Pedro Bornia <borniaph@gmail.com>
2024-09-08 22:41:39 -0300

    Update botão voltar

--- src/components/Perfil.js
+++ src/components/Perfil.js
@@ -5,10 +5,8 @@ import { Link } from "react-router-dom";
 function Perfil() {
     return(
         <div className="perfil-container">
-            <div className="perfil-header">
-                <button className="home-button">
-                    <Link to="/dashboard">Home</Link>
-                </button>
+            <div className="perfil-header">   
+                <Link to="/dashboard" className="voltar-button">Voltar</Link>
                 <h2 className="perfil-titulo">Perfil</h2>
             </div>
 
