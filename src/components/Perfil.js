6d0ac1f0425772616cb0be2f35b8e0a8cbedcc61
Tiyo <eduardotiyo0@gmail.com>
2024-09-08 23:08:09 -0300

    alterando usuario

--- src/components/Perfil.js
+++ src/components/Perfil.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "../styles/perfil.css";
 import { Link } from "react-router-dom";
 
@@ -7,10 +7,64 @@ function Perfil() {
     const [isPasswordModalOpen, setIsPasswordModalOpen] = useState(false);
     const [isAddBalanceModalOpen, setIsAddBalanceModalOpen] = useState(false);
     const [isDeleteAccountModalOpen, setIsDeleteAccountModalOpen] = useState(false);
+    const [usuario, setUsuario] = useState("");
+    const [telefone, setTelefone] = useState("");
+
+    const validateUser = async() => {
+        const usuario = await getSession();
+        if(!usuario){
+          alert("É necessário estar logado para acessar essa tela");
+          window.location.href = "http://localhost:3000/Login";
+        }
+    }
+
+    const getSession = async () =>{
+        const url = "http://localhost:3500/login/session";
+    
+        const response = await fetch(url, {credentials: "include"});
+    
+        const json = await response.json();
+        setUsuario(json.user.usuario);
+        setTelefone(json.user.celular);
+        
+        return json.user;
+      }
+      useEffect(() =>{
+        validateUser(); 
+    }, []);
+
+    const handleAtualizarCadastro = async (e) => {
+        e.preventDefault();
+
+        const url = `http://localhost:3500/cadastro`;
+
+        fetch(url, {
+            method: "PUT",
+            credentials: "include",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                usuario: usuario,
+                celular: telefone
+            }),
+        })
+        .then(async (response) => {
+            if(!response.ok){
+                const body = await response.json();
+                alert(body.message);
+                return;
+            }
+
+            alert("Perfil atualizado com sucesso!");
+        });
+        
+    };
 
     // Funções para abrir e fechar as modais
     const openPasswordModal = () => {
         setIsPasswordModalOpen(true);
+        
     };
 
     const closePasswordModal = () => {
@@ -44,16 +98,20 @@ function Perfil() {
 
             <form action="#" className="perfil-form">
                 <input
+                    onChange={(e) => setUsuario(e.target.value)}
                     type="text"
                     placeholder="Usuário"
                     className="perfil-info"
+                    value={usuario}
                 />
                 <input
+                    onChange={(e) => setTelefone(e.target.value)}
                     type="tel"
                     placeholder="Telefone"
                     className="perfil-info"
+                    value={telefone}
                 />
-                <button className="perfil-button">
+                <button className="perfil-button" onClick={handleAtualizarCadastro}>
                     Salvar alterações
                 </button>
             </form>
