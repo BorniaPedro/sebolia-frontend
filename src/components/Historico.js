35773cd97d7bf0395230afaa2df1217bfd745af6
viictor1 <victorgaspalmeida@hotmail.com>
2024-09-08 21:52:45 -0300

    feat: listagem historico

--- src/components/Historico.js
+++ src/components/Historico.js
@@ -1,22 +1,62 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "../styles/historico.css"
 
 function Historico() {
+    const [transactions, setTransactions] = useState([]);
+
+    const getHistorico = async () => {
+      const url = `http://localhost:3500/historico`;
+  
+      const response = await fetch(url, {credentials: "include"});
+    
+      const json = await response.json();
+      setTransactions(json);
+    }
+  
+    const getSession = async () =>{
+      const url = "http://localhost:3500/login/session";
+  
+      const response = await fetch(url, {credentials: "include"});
+  
+      const json = await response.json();
+      return json.user;
+    }
+  
+    const validateUser = async() => {
+      const usuario = await getSession();
+      if(!usuario){
+        alert("É necessário estar logado para acessar essa tela");
+        window.location.href = "http://localhost:3000/Login";
+      }
+    }
+  
+  
+    useEffect(() => {
+      validateUser();
+      getHistorico();
+    }, []);
+
     return(
         <div className="historico-container">
             <h2 className="historico-titulo">Histórico do usuário</h2>
 
             <table className="historico-table">
-                <tr className="historico-info">
-                    <div className="historico-livroTitulo">Livro 1</div>
-                    <div className="historico-livroInfo">
-                        <span className="historico-livroEstado">Usado</span>
-                        <span className="historico-livroPreco">R$12,90</span>
-                        <span className="historico-livroData">06/09/2024</span>
-                        <span className="historico-tipo">Compra</span>
-                    </div>
-                </tr>
-            </table>
+                <tbody>
+                    {transactions.map((t, index) => (
+                    <tr className="historico-info" key={index}>
+                        <td className="historico-livroTitulo">{t.tituloLivro}</td>
+                        <td className="historico-livroInfo">
+                            <span className="historico-livroEstado">Estado: {t.estado}</span>
+                            <span className="historico-livroPreco">Preço: R${t.preco}</span>
+                            <span className="historico-livroData">Data: {new Date(t.data).toISOString().split('T')[0]}</span>
+                            <span className={"historico-tipo " + (t.tipo === "Compra" ? "verde" : "vermelho")} >Tipo: <span>{t.tipo}</span></span>
+
+                        </td>
+                    </tr>
+                    ))}
+                </tbody>
+                </table>
+
         </div>
 
     )
